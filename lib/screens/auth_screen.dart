82323e1caf1341d666349eaf0b9b0949709c98d0
rajkumararisetty <rajkumar.arisetty@preqin.com>
2020-03-16 17:26:21 +0530

    AnimatedBuild alternative

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -120,7 +120,7 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
         360,
       ),
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.linear,));
-    _heightAnimation.addListener(() => setState(() {}));
+    //_heightAnimation.addListener(() => setState(() {}));
     super.initState();
   }
 
@@ -206,15 +206,19 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 360 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-            // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 360 : 260),
-            BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
-        child: Form(
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, child) => Container(
+          // height: _authMode == AuthMode.Signup ? 360 : 260,
+          height: _heightAnimation.value.height,
+          constraints:
+              // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 360 : 260),
+              BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: child
+        ),
+        child:  Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
